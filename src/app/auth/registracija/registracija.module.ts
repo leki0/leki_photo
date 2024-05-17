600f28fe4ca436fd78fa05ba44736207b57f6a13
leki0 <aleksastancic9@gmail.com>
2024-05-17 23:52:21 +0200

    validacija za registraciju/ nije skroz uradjena

--- src/app/auth/registracija/registracija.module.ts
+++ src/app/auth/registracija/registracija.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,7 +13,8 @@ import { RegistracijaPage } from './registracija.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    RegistracijaPageRoutingModule
+    RegistracijaPageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [RegistracijaPage]
 })
--- src/app/auth/registracija/registracija.page.ts
+++ src/app/auth/registracija/registracija.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registracija',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registracija.page.scss'],
 })
 export class RegistracijaPage implements OnInit {
-
+  registerForm!: FormGroup;
   constructor() { }
 
   ngOnInit() {
+    this.registerForm=new FormGroup({
+      ime:new FormControl("Aleksa",Validators.required),
+      prezime:new FormControl(null,Validators.required),
+      email:new FormControl(null,(Validators.required,Validators.email)),
+      password:new FormControl(null,(Validators.required)),
+      username:new FormControl(null)
+
+    });
+    onRegister(){
+
+    }
   }
 
 }
