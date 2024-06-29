9fd29f6a7c9dcffad73e924826fd8778c7d9a801
leki0 <aleksastancic9@gmail.com>
2024-06-29 16:19:28 +0200

    Implementirana registracija na formi prijave, dodata i opcija da ne mogu dva ista userera da se registruju

--- src/app/auth/prijava/prijava.page.ts
+++ src/app/auth/prijava/prijava.page.ts
@@ -12,30 +12,34 @@ import { AlertController } from '@ionic/angular';
 export class PrijavaPage implements OnInit {
   isLoading = false;
 
-  constructor(private authService: AuthService, private router: Router, private alertCtrl: AlertController) { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private alertCtrl: AlertController
+  ) {}
 
-  ngOnInit() {
+  ngOnInit() {}
 
-  }
   OnPrijava(form: NgForm) {
     this.isLoading = true;
     console.log(form);
     if (form.valid) {
-      this.authService.prijava(form.value).subscribe(resData => {
-        console.log("Prijava uspjesna!");
-        console.log(resData);
-        this.isLoading = false;
-        this.router.navigateByUrl('usluge/tabs/istrazi');
-      },
-        errRes => {
+      this.authService.prijava(form.value).subscribe(
+        (resData) => {
+          console.log("Prijava uspješna!");
+          console.log(resData);
+          this.isLoading = false;
+          this.router.navigateByUrl('usluge/tabs/istrazi');
+        },
+        (errRes) => {
           console.log(errRes);
           this.isLoading = false;
           let message = "Nisu dobro unijeti email ili lozinka";
           const code = errRes.error.error.message;
           if (code === "EMAIL_NOT_FOUND") {
-            message = "Email adresa nije pronadjena!";
+            message = "Korisnik nije registrovan, morate prvo da se registrujete!";
           } else if (code === "INVALID_PASSWORD") {
-            message = "Lozinka nije ispravno unijeta!"
+            message = "Lozinka nije ispravno unijeta!";
           }
 
           this.alertCtrl.create({
@@ -44,14 +48,12 @@ export class PrijavaPage implements OnInit {
             buttons: ["Okay"]
           }).then((alert) => {
             alert.present();
-          })
-          form.reset();//MOZE BITI DA JE GRESKAAA KOD NJE JE LOGINFORM.
+          });
+          form.reset();
         }
       );
+    } else {
+      this.isLoading = false;
     }
-
-
   }
-
-
 }
--- src/app/auth/registracija/registracija.page.ts
+++ src/app/auth/registracija/registracija.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../auth.service';
-import { LoadingController } from '@ionic/angular';
+import { LoadingController, AlertController } from '@ionic/angular';
 import { Router } from '@angular/router';
 
 @Component({
@@ -11,32 +11,60 @@ import { Router } from '@angular/router';
 })
 export class RegistracijaPage implements OnInit {
   registerForm!: FormGroup;
-  constructor(private authService: AuthService, private loadingCtrl: LoadingController, private router: Router) { }
+
+  constructor(
+    private authService: AuthService,
+    private loadingCtrl: LoadingController,
+    private alertCtrl: AlertController,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.registerForm = new FormGroup({
-      ime: new FormControl("Aleksa", Validators.required),
+      ime: new FormControl('Aleksa', Validators.required),
       prezime: new FormControl(null, Validators.required),
-      email: new FormControl(null, (Validators.required, Validators.email)),
-      password: new FormControl(null, (Validators.required, Validators.minLength(7))),
-
-
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(7)]),
     });
-
   }
+
   onRegister() {
-    this.loadingCtrl.
-      create({ message: "Registering..." }).
-      then((loadingEl) => {
-        loadingEl.present();
-        this.authService.registracija(this.registerForm.value).subscribe(resData => {
-          console.log("Registracija uspjesna!");
+    this.loadingCtrl.create({ message: 'Registering...' }).then((loadingEl) => {
+      loadingEl.present();
+      this.authService.registracija(this.registerForm.value).subscribe(
+        (resData) => {
+          console.log('Registracija uspjesna!');
           console.log(resData);
           loadingEl.dismiss();
           this.router.navigateByUrl('/usluge');
-        });
-      });
-
+        },
+        (errRes) => {
+          loadingEl.dismiss();
+          const code = errRes.error.error.message;
+          let message = 'Došlo je do greške prilikom registracije.';
+          if (code === 'EMAIL_EXISTS') {
+            message = 'Korisnik sa ovom email adresom već postoji!';
+          }
+          this.alertCtrl
+            .create({
+              header: 'Registracija nije uspjela',
+              message: message,
+              buttons: [
+                {
+                  text: 'U redu',
+                  handler: () => {
+                    if (code === 'EMAIL_EXISTS') {
+                      this.router.navigateByUrl('/prijava');
+                    }
+                  },
+                },
+              ],
+            })
+            .then((alertEl) => {
+              alertEl.present();
+            });
+        }
+      );
+    });
   }
-
 }
